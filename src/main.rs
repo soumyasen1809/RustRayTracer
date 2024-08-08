e8b2d5afb8e3773aade4e764f7064dbdebfd1575
Soumya <soumyasen1809@gmail.com>
2024-08-08 17:53:14 +0200

    Minor fixes to the ppm output
    - Minor changes to the header values
    
    %SOFTWARE

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
     let mut file = File::create(&file_path).unwrap();
 
     writeln!(file, "P3\n{} {}\n255", image_width, image_height).unwrap();
-    for x_index in 0..image_height - 1 {
-        for y_index in 0..image_width - 1 {
+    for y_index in 0..image_height {
+        for x_index in 0..image_width {
             let r = x_index as f64 / (image_width - 1) as f64;
             let g = y_index as f64 / (image_height - 1) as f64;
             let b = 0.0;
