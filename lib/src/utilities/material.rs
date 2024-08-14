1207b338b24d382c1cec34345bfbe156845d1177
Soumya <soumyasen1809@gmail.com>
2024-08-14 17:26:07 +0200

    [10.6] Fuzzy Reflection
    - Randomized the reflection from the metal
    using a fuzz factor
    
    %SOFTWARE

--- lib/src/utilities/material.rs
+++ lib/src/utilities/material.rs
@@ -50,11 +50,15 @@ impl Material for Lambertian {
 #[derive(Clone)]
 pub struct Metal {
     albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Color) -> Self {
-        Self { albedo: albedo }
+    pub fn new(albedo: Color, fuzz: f64) -> Self {
+        Self {
+            albedo,
+            fuzz: if fuzz < 1.0 { fuzz } else { 1.0 },
+        }
     }
 }
 
@@ -62,6 +66,7 @@ impl Default for Metal {
     fn default() -> Self {
         Self {
             albedo: Color::new(0.8, 0.8, 0.8),
+            fuzz: 1.0,
         }
     }
 }
@@ -69,13 +74,19 @@ impl Default for Metal {
 impl Material for Metal {
     fn scatter(&self, incoming_ray: Ray, record: &HitRecord) -> Option<Scatter> {
         // Metal material with reflectance function
-        let reflect_direction: Vector3 = incoming_ray
+        let reflect_direction: Vector3 = (incoming_ray
             .get_direction()
             .unit_vector()
-            .reflection(&record.normal);
-        Some(Scatter {
-            scattered_ray: Ray::new(record.point, reflect_direction),
-            attenuation: self.albedo,
-        })
+            .reflection(&record.normal))
+            + (Vector3::random_unit_vector() * self.fuzz);
+        let scattered_ray = Ray::new(record.point, reflect_direction);
+        let attenuation = self.albedo;
+        if scattered_ray.get_direction().dot_prod(record.normal) > 0.0 {
+            return Some(Scatter {
+                scattered_ray,
+                attenuation,
+            });
+        }
+        None
     }
 }
--- project_bin/src/main.rs
+++ project_bin/src/main.rs
@@ -15,12 +15,12 @@ fn main() {
     // https://raytracing.github.io/books/RayTracingInOneWeekend.html
 
     // World
-    let mut world: Vec<Box<dyn Hittable>> = Vec::with_capacity(11 * 11);
+    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
 
     let material_ground = Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
     let material_center = Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
-    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
+    let material_left = Box::new(Metal::new(Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_right = Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.push(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
